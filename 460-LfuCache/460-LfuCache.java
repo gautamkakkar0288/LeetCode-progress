9c4179b145b6014c06afeada50ea5caac9f0dd2d
Gautam Kakkar <gautamarora0288@gmail.com>
2026-08-04 23:53:54 +0530

    Create 460-LfuCache

---
+++ 460-LfuCache/460-LfuCache.java
@@ -0,0 +1,117 @@
+// Last updated: 04/08/2026, 23:53:54
+class LFUCache {
+
+    class Node {
+        int key, value, freq;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.freq = 1;
+        }
+    }
+
+    class DoublyLinkedList {
+        Node head, tail;
+        int size;
+
+        DoublyLinkedList() {
+            head = new Node(0, 0);
+            tail = new Node(0, 0);
+            head.next = tail;
+            tail.prev = head;
+            size = 0;
+        }
+
+        void addFirst(Node node) {
+            node.next = head.next;
+            node.prev = head;
+            head.next.prev = node;
+            head.next = node;
+            size++;
+        }
+
+        void remove(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            size--;
+        }
+
+        Node removeLast() {
+            if (size == 0) return null;
+            Node node = tail.prev;
+            remove(node);
+            return node;
+        }
+    }
+
+    private int capacity;
+    private int minFreq;
+
+    private Map<Integer, Node> cache;
+    private Map<Integer, DoublyLinkedList> freqMap;
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        this.minFreq = 0;
+        cache = new HashMap<>();
+        freqMap = new HashMap<>();
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key))
+            return -1;
+
+        Node node = cache.get(key);
+        updateFreq(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        if (capacity == 0)
+            return;
+
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            updateFreq(node);
+            return;
+        }
+
+        if (cache.size() == capacity) {
+            DoublyLinkedList list = freqMap.get(minFreq);
+            Node remove = list.removeLast();
+            cache.remove(remove.key);
+        }
+
+        Node node = new Node(key, value);
+        minFreq = 1;
+
+        DoublyLinkedList list = freqMap.getOrDefault(1, new DoublyLinkedList());
+        list.addFirst(node);
+        freqMap.put(1, list);
+
+        cache.put(key, node);
+    }
+
+    private void updateFreq(Node node) {
+
+        int freq = node.freq;
+
+        DoublyLinkedList list = freqMap.get(freq);
+        list.remove(node);
+
+        if (freq == minFreq && list.size == 0)
+            minFreq++;
+
+        node.freq++;
+
+        DoublyLinkedList newList =
+                freqMap.getOrDefault(node.freq, new DoublyLinkedList());
+
+        newList.addFirst(node);
+        freqMap.put(node.freq, newList);
+    }
+}
